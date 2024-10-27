f9c2ee415db6b3329190be1166160ab05d82ea45
YS5G <yasir.gva@gmail.com>
2024-10-27 17:27:34 -0400

    feat: add a nix flake and a home manager module

---
+++ nix/module.nix
@@ -0,0 +1,17 @@
+{config, lib, ...}:
+let
+  inherit (config.programs.matugen-themes) presets;
+
+  templateData = import ./parseReadme.nix { inherit config lib; };
+  
+  templates = builtins.listToAttrs (map (name: lib.nameValuePair name templateData.${name}) presets);  
+in {
+  options.programs.matugen-themes.presets = with lib; mkOption {
+    type = types.listOf (types.enum (builtins.attrNames templateData));
+    default = [];
+    description = "The themes to use";
+  };
+  config = lib.mkIf (presets != []) {
+    programs.matugen.settings = { inherit templates; };
+  };
+}
